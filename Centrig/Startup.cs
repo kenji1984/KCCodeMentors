6e7a4e57e1306761e1563635981e88a1d91e0ac7
kenji1984 <kenji84@outlook.com>
2015-06-17 19:57:19 -0500

    First Commit

---
+++ Centrig/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Centrig.Startup))]
+namespace Centrig
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
